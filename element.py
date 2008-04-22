c77239dfc653a2f45bc7056fa717df43d47ae973
Hugo Arts <hugo.yoshi@gmail.com>
2008-04-22 16:58:33 +0200

    added create_element and hide methods to Element class

--- element.py
+++ element.py
@@ -8,5 +8,24 @@ class Element(std.sprite.Sprite):
     """Element - the base class for all GUI elements"""
 
     def __init__(self, style):
+        """initialize the element."""
         std.sprite.Sprite.__init__(self)
         self.style = style
+        self.hidden = False
+
+    def create_element(self):
+        """create the element.
+
+        This must be called before the element can be rendered to the
+        screen. It renders the element surface. Must be implemented by
+        derived classes
+        """
+        raise NotImplementedError("create_element is not implemented in class '%s'" % type(self).__name__)
+
+    def hide(self, value=True):
+        """hide/unhide the GUI Element
+
+        Passing in True hides the element, preventing it from
+        rendering or responding to events. False unhides again.
+        """
+        self.hidden = value
