766684f58c3d2076ac12a734c4e174e4ddced560
Hugo Arts <hugo.yoshi@gmail.com>
2008-06-25 22:16:36 +0200

    added support for composite content replacement, non-leaf composite nodes, and reworked composite file

--- composite.py
+++ composite.py
@@ -4,39 +4,57 @@
 
 import xml.dom.minidom as minidom
 import os
-import goo.parser
+import goo.parser, goo.containers
 
 file_path = ""
-COMPOSITES = {}
 
-def get(tagname):
-    """load composite widget
+class Composite(goo.containers.Sizer):
+    """Responsible for loading widgets from xml files and using them as children"""
 
-    load the first widget in the file registered under tagname
+    def __init__(self, parent, children, source, **attributes):
+        """initialize this composite widget"""
+        self.childnodes = children
+        children = goo.parser.get_root(os.path.join(file_path, source)).childNodes
+        goo.containers.Sizer.__init__(self, parent, children, **attributes)
+
+
+class Content(object):
+    """return tag from original file with type and name
+
+    this tag is found in composite files. It is replaced by a tag (whose tagname must be equal to the type attribute)
+    from one of the child nodes of the composite tag, as used in the file importing the composite file.
+    note that content tags are replaced with the original tags in order, e.g. the first content tag in the composite file is
+    replaced with the first tag in the original file.
     """
-    if tagname not in COMPOSITES:
-        raise ParseError("encountered invalid tag: '%s'" % node.tagName)
+    def __new__(cls, parent, tagtype=None, **attributes):
+        composite = cls.find_composite(parent)
+        node = cls.find_firstnode(composite)
+        if node.tagName != tagtype:
+            raise RuntimeError("tag to replace Content tag has wrong type: '%s'" % node.tagName)
 
-    xml_file = os.path.join(file_path, COMPOSITES[tagname])
-    dom = minidom.parse(xml_file)
-    node = dom.documentElement.firstChild
+        widget = goo.parser.get_widget(node)
+        new_attributes = goo.parser.get_attributes(node)
+        attributes.update(new_attributes)
 
-    while node.nodeType == 3:
-        node = node.nextSibling
+        node.parentNode.removeChild(node)
 
-    #recurse back into get_widget until we find some built-in widgets
-    return goo.parser.get_widget(node)[0], node
+        if node.hasChildNodes():
+            return widget(parent, node.childNodes, **attributes)
+        else:
+            return widget(parent, **attributes)
 
-def register(name, xml_file):
-    """register new composite tag
+    @staticmethod
+    def find_composite(parent):
+        """find the composite method in the hierarchy"""
+        while not isinstance(parent, Composite):
+            parent = parent.parent
+            if isinstance(parent, goo.NullParent):
+                raise RuntimeError("Content tag used in non-composite file")
+        return parent
 
-    register a new composite tag, so it will be recognised if it appears in
-    your files.
-    """
-    for index, arg in enumerate((name, xml_file)):
-        if not isinstance(arg, basestring):
-            raise TypeError("register() argument %d must be string not %s" % (index, type(arg).__name__))
-        elif arg == "":
-            raise ValueError("invalid argument %d for register: '%s'" % (index, arg))
-
-    COMPOSITES[name] = xml_file
+    @staticmethod
+    def find_firstnode(parent):
+        """find the first non-text node in the node list"""
+        for node in parent.childnodes:
+            if node.nodeType != 3:
+                return node
