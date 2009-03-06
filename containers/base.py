0aba36c9593b299b75504d99c130b5a293b865ea
Hugo Arts <hugo.yoshi@gmail.com>
2009-03-06 00:06:02 -0600

    added margin and padding attributes to container objects, to avoid accessing the style element all the time

--- containers/base.py
+++ containers/base.py
@@ -17,12 +17,11 @@ class Container(goo.element.Element):
     elements and provides a background image. The box is not resizable,
     but it is nestable.
     """
-    def __init__(self, parent, children, size=(0, 0), **attributes):
+    def __init__(self, parent, children, **attributes):
         """initialize container"""
         goo.element.Element.__init__(self, parent, **attributes)
-        self.rect.size = size if size == (0, 0) else tuple(int(e) for e in size.split(','))
-        if not (hasattr(self, 'min_width') and hasattr(self, 'min_height')):
-            self.min_width, self.min_height = (0, self.style['padding'] - self.style['margin'])
+        self.margin, self.padding = self.style['margin'], self.style['padding']
+        self.min_width = self.min_height = self.padding
 
         self.children = [goo.parser.parse(node, self) for node in children if node.nodeType not in (3, 8)]
         self.children = filter(lambda x: x is not None, self.children)
@@ -36,15 +35,15 @@ class Container(goo.element.Element):
         Within that rectangle, the child is free to position itself. When done, the child must return a rectangle
         representing the actual space it is occupying, so that the container can give a suitable area to the next child
         """
-        self.nextchild_pos = [self.style['padding'], self.style['padding']]
+        self.nextchild_pos = [self.padding, self.padding]
         for i, child in enumerate(self.children):
-            width = self.rect.width - self.nextchild_pos[0] - self.style['padding']
+            width = self.rect.width - self.nextchild_pos[0] - self.padding
 
             height = self.rect.height - self.nextchild_pos[1] - sum(c.rect.height for c in self.children[i+1:])
-            height -= (len(self.children[i+1:]) * self.style['margin']) + self.style['padding']
+            height -= (len(self.children[i+1:]) * self.margin) + self.padding
 
             area = child.arrange(pygame.Rect(self.nextchild_pos, (width, height)))
-            self.nextchild_pos[1] = (area.bottom + self.style['margin'])
+            self.nextchild_pos[1] = (area.bottom + self.margin)
 
     def adjust(self, child):
         """adjust the container to accomodate a child.
@@ -52,14 +51,16 @@ class Container(goo.element.Element):
         many container attributes (for example, the minimum container dimensions) depend on the children inside it.
         Therefore, this function should be called with the child after the child has been created.
         """
-        if child.rect.width + self.style['padding'] > self.min_width:
-            self.min_width = child.rect.width + self.style['padding']
-        self.min_height += (child.rect.height + self.style['margin'])
+        if child.rect.width + self.padding > self.min_width:
+            self.min_width = child.rect.width + self.padding
+        self.min_height += (child.rect.height + self.margin)
 
     def create(self):
         """render the container sprite."""
-        self.min_width += self.style['padding']
-        self.min_height += self.style['padding']
+        goo.element.Element.create(self)
+
+        self.min_width += self.padding
+        self.min_height += self.padding - self.margin
         if self.rect.width < self.min_width:
             self.rect.width = self.min_width
         if self.rect.height < self.min_height:
@@ -88,12 +89,6 @@ class Container(goo.element.Element):
 class HrContainer(Container):
     """HorizontalContainer - similar to the basic container, but it arranges children horizontally"""
 
-    def __init__(self, parent, children, size=(0, 0), **attributes):
-        """initialize HrContainer"""
-        goo.element.Element.__init__(self, parent, **attributes)
-        self.min_width, self.min_height = (self.style['padding'] - self.style['margin'], 0)
-        Container.__init__(self, parent, children, size, **attributes)
-
     def arrange_children(self):
         """arrange the children of this container
 
@@ -101,15 +96,15 @@ class HrContainer(Container):
         Within that rectangle, the child is free to position itself. When done, the child must return a rectangle
         representing the actual space it is occupying, so that the container can give a suitable area to the next child
         """
-        self.nextchild_pos = [self.style['padding'], self.style['padding']]
+        self.nextchild_pos = [self.padding, self.padding]
         for i, child in enumerate(self.children):
-            height = self.rect.height - self.nextchild_pos[1] - self.style['padding']
+            height = self.rect.height - self.nextchild_pos[1] - self.padding
 
             width = self.rect.width - self.nextchild_pos[0] - sum(c.rect.width for c in self.children[i+1:])
-            width -= (len(self.children[i+1:]) * self.style['margin']) + self.style['padding']
+            width -= (len(self.children[i+1:]) * self.margin) + self.padding
 
             area = child.arrange(pygame.Rect(self.nextchild_pos, (width, height)))
-            self.nextchild_pos[0] = (area.right + self.style['margin'])
+            self.nextchild_pos[0] = (area.right + self.margin)
 
     def adjust(self, child):
         """adjust the container to accomodate a child.
@@ -117,8 +112,19 @@ class HrContainer(Container):
         many container attributes (for example, the minimum container dimensions) depend on the children inside it.
         Therefore, this function should be called with the child after the child has been created.
         """
-        if child.rect.height + self.style['padding'] > self.min_height:
-            self.min_height = child.rect.height + self.style['padding']
-        self.min_width += (child.rect.width + self.style['margin'])
+        if child.rect.height + self.padding > self.min_height:
+            self.min_height = child.rect.height + self.padding
+        self.min_width += (child.rect.width + self.margin)
 
+    def create(self):
+        """render the container sprite."""
+        goo.element.Element.create(self)
 
+        self.min_width += self.padding - self.margin
+        self.min_height += self.padding
+        if self.rect.width < self.min_width:
+            self.rect.width = self.min_width
+        if self.rect.height < self.min_height:
+            self.rect.height = self.min_height
+
+        self.parent.adjust(self)
